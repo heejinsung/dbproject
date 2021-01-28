5ba4f639491f7f5815564f1cb293d31c01c278ae
heejin <shj683706@gmail.com>
2021-01-28 14:48:03 +0900

    1/28 관리자답변 완성

--- Goldd/pages/qna/qna_view.php
+++ Goldd/pages/qna/qna_view.php
@@ -95,19 +95,64 @@
               } else {
               ?>
               <button type="submit" class=""><a href="/Goldd/pages/qna/qna.php">돌아가기</a></button>
-              <button type="submit" class="ansSubmit">수정</button>
+              <button type="submit" class="ansUpdate" onclick="ansUpdate()">수정</button>
               <?php  
               }
               ?>
               
             </div>
             <!-- end of write Box -->
+            
+            <?php
+            $ans_num = $_GET['num'];
+            //database connect
+            include  $_SERVER['DOCUMENT_ROOT']."/Goldd/php_process/connect/db_connect.php"; 
+            $sql="select * from GOLD_ANS where GOLD_ANS_QNA_num=$ans_num order by GOLD_ANS_num desc";
+
+            $rep_result=mysqli_query($dbConn, $sql);
+            
+
+            while($rep_row=mysqli_fetch_array($rep_result)){
+              $rep_con=$rep_row['GOLD_ANS_con'];
+              $rep_ans_num=$rep_row['GOLD_ANS_num'];
+            ?>
+
+            <div class="ansResult">
+              <p class="adminId">
+                <b>관리자 답변</b>
+                <?php
+                if ($userlevel == 1){
+                ?>
+                <a href="/Goldd/php_process/pages/ans_delete.php?num=<?=$rep_ans_num?>" class="ansDeleteBtn">삭제</a>
+                <?php
+                }
+                ?>
+
+              </p>
+              <p class="ansResultTxt"><?=$rep_con?></p>
+
+            </div>
+
+            <?php
+            }
+            ?>
 
             <div class="answerBox">
-                <form action="" method="post" name="ansInputForm" class="ansInputForm">
+                <form action="/Goldd/php_process/pages/ans_insert.php?num=<?=$ans_num?>" method="post" name="ansInputForm" class="ansInputForm">
                     <textarea name="ansInputTxt" placeholder="답글을 작성해 주세요."></textarea>
                     <p class="ansBtnBox">
-                        <button type="submit" class="ansBtn">답글달기</button>
+                      <?php
+                      if($userid==''){
+                      ?>  
+                      <button type="button" class="ansBtn" onclick="plzLogin()">답글달기</button>
+                      <?php  
+                      } else {
+                      ?>
+                      <button type="button" class="ansBtn" onclick="reply()">답글달기</button>
+                      <?php  
+                      }
+                      ?>  
+                        <!-- <button type="button" class="ansBtn" onclick="reply()">답글달기</button> -->
                     </p>
                     
                 </form>
@@ -129,14 +174,15 @@
     <script src="/Goldd/js/custom.js"></script>
     <script src="/Goldd/js/web_design_page.js"></script>
     <script>
-        const ansSubmit = document.querySelector(".ansSubmit");
-        ansSubmit.addEventListener('click', insertAns);
+        // const ansBtn = document.querySelector(".ansBtn");
+        // ansBtn.addEventListener('click', insertAns);
 
         function plzLogin(){
             alert('글쓰기를 하시려면 로그인이 필요합니다.');
         }
 
-        function insertAns(){
+        function ansUpdate(){
+
         if(!document.ansForm.ansTitle.value){
             alert("제목을 입력해 주세요.");
             document.ansForm.ansTitle.focus();
@@ -150,6 +196,16 @@
         }
 
         document.ansForm.submit();
+        }
+
+        function reply(){
+          if(!document.ansInputForm.ansInputTxt.value){
+            alert("내용을 입력해 주세요.");
+            document.ansInputForm.ansInputTxt.focus();
+            return ;
+          }
+
+          document.ansInputForm.submit();
         }
     </script>
   </body>
