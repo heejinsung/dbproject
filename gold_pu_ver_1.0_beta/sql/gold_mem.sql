0139f6d651e73c52618c3ae1cb681c43d8f6bf66
heejin <shj683706@gmail.com>
2021-02-04 12:30:24 +0900

    2/4 db project 1차 베타 퍼블리싱

---
+++ gold_pu_ver_1.0_beta/sql/gold_mem.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- 생성 시간: 21-02-04 03:41
+-- 서버 버전: 10.4.17-MariaDB
+-- PHP 버전: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `gold_mem`
+--
+
+CREATE TABLE `gold_mem` (
+  `GOLD_mem_num` int(11) NOT NULL COMMENT '고유번호',
+  `GOLD_mem_id` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '아이디',
+  `GOLD_mem_name` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '이름',
+  `GOLD_mem_email` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '이메일',
+  `GOLD_mem_pass` varchar(80) CHARACTER SET utf8 NOT NULL COMMENT '비밀번호',
+  `GOLD_mem_regi_day` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '가입일',
+  `GOLD_mem_level` int(11) DEFAULT NULL COMMENT '레벨(회원등급)',
+  `GOLD_mem_point` int(11) NOT NULL COMMENT '포인트'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 테이블의 덤프 데이터 `gold_mem`
+--
+
+INSERT INTO `gold_mem` (`GOLD_mem_num`, `GOLD_mem_id`, `GOLD_mem_name`, `GOLD_mem_email`, `GOLD_mem_pass`, `GOLD_mem_regi_day`, `GOLD_mem_level`, `GOLD_mem_point`) VALUES
+(2, 'root', 'heejin', 'shj0620@naver.com', '', '2020-12-28 09:38:12', 9, 0),
+(3, 'shj', 'heejin', 'shj0620@naver.com', '1234', '2020-12-28 09:38:27', 1, 0),
+(10, 'shj6837', '성희진', 'shj0620@naver.com', '12345', '2020-12-28 10:09:41', 5, 100),
+(12, 'goldkk', '김골드', 'gold123@gmail.com', 'gold11', '2021-02-02 03:52:57', 9, 0);
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `gold_mem`
+--
+ALTER TABLE `gold_mem`
+  ADD PRIMARY KEY (`GOLD_mem_num`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `gold_mem`
+--
+ALTER TABLE `gold_mem`
+  MODIFY `GOLD_mem_num` int(11) NOT NULL AUTO_INCREMENT COMMENT '고유번호', AUTO_INCREMENT=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
